de1f7f897a7ee6a185b0818b3fda78e389caddb7
Samuel Jirénius <samuel@jirenius.com>
2018-06-24 21:58:35 +0200

    Exported sortOrderCompare function.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import Collection from './Collection.js';
+import Collection, { sortOrderCompare } from './Collection.js';
 import CollectionWrapper from './CollectionWrapper.js';
 import Model from './Model.js';
 import ModifyModel from './ModifyModel.js';
 import SortedMap from './SortedMap.js';
 
-export { Collection, CollectionWrapper, Model, ModifyModel, SortedMap };
+export { Collection, CollectionWrapper, Model, ModifyModel, SortedMap, sortOrderCompare };
